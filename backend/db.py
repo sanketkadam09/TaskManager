a9d97189039d2cd79aee0ca14509fb59d2de0487
Sanket Kadam <sk8185710@gmail.com>
2026-03-10 10:51:15 +0530

    update yml file 8 time

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/taskdb"
+# Use environment variable if set (Docker), else fallback to localhost
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/taskdb")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
